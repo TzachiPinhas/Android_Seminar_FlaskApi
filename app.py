94e5d11e1de91d715170f653f3824bf896c9a4b1
TzachiPinhas <tzachi2468@gmail.com>
2025-01-16 01:39:50 +0200

    add host=0.0.0.0

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ initial_routes(app)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', debug=True, port=port)

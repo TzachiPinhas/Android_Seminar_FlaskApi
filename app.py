9d8b0b3cff0f2a555e77a7500b4fef148e27f9bf
TzachiPinhas <tzachi2468@gmail.com>
2025-01-16 02:02:07 +0200

    add route

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ MongoConnectionHolder.initialize_db()
 # רישום הנתיבים
 initial_routes(app)
 
+@app.route('/')
+def home():
+    return "Welcome to the Flask API! The server is running."
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', debug=True, port=port)

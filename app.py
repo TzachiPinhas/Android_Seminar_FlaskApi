c56a7d1d71c6d843817ad1406ca015930b5732fe
TzachiPinhas <tzachi2468@gmail.com>
2025-01-26 14:36:07 +0200

    fix

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flasgger import Swagger
-from flask_cors import CORS  # הוספת התמיכה ב-CORS
 from mongodb_connection_manager import MongoConnectionHolder
 from routes import initial_routes
 import os
 
 app = Flask(__name__)
-CORS(app)  # הפעלת CORS לכל הבקשות
 Swagger(app)
 
 MongoConnectionHolder.initialize_db()
@@ -16,6 +14,7 @@ initial_routes(app)
 @app.route('/')
 def home():
     return "Welcome to the Flask API! The server is running."
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
